6c6abb0ee304bc0cb65ae6a48aa522cf9391e4d7
Alexis Rosquete <alexis.rosquete@gmail.com>
2024-08-05 11:41:45 +0000

    hola

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,47 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.first_name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
             # do not serialize the password, its a security breach
         }
+    
+class Psicologo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.Integer, nullable=False)
+    titutlo = db.Column(db.String(1000), nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.first_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+            # do not serialize the password, its a security breach
+        }
+    
+class TokenBlockedList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(100), nullable=False)
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,21 +2,124 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from flask_jwt_extended import create_access_token, get_jwt_identity, get_jwt, jwt_required
+from api.models import db, User, TokenBlockedList, Psicologo
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route('/signup/user', methods=['POST'])
+def user_signup():
+    body=request.get_json()
+    if "email" not in body:
+        return jsonify({"msg":"El campo email es requerido"}), 400
+    if "password" not in body:
+        return jsonify({"msg":"El campo password es requerido"}), 400
+    
+    encrypted_password=bcrypt.generate_password_hash(body["password"]).decode('utf-8') 
+    
+    new_user = User(email=body["email"],  
+                    password=encrypted_password, is_active=True)
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    if "first_name" in body:
+        new_user.first_name = body["first_name"]
+    else:
+        new_user.first_name = ""
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if "last_name" in body:
+        new_user.last_name = body["last_name"]
+    else:
+        new_user.last_name = ""
 
-    return jsonify(response_body), 200
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"msg":"ok"})
+
+@api.route('/signup/psico', methods=['POST'])
+def user_signup_psico():
+    body=request.get_json()
+    if "email" not in body:
+        return jsonify({"msg":"El campo email es requerido"}), 400
+    if "password" not in body:
+        return jsonify({"msg":"El campo password es requerido"}), 400
+    if "role" not in body:
+        return jsonify({"msg": "El Campo role es requerido"}), 400
+    
+    encrypted_password=bcrypt.generate_password_hash(body["password"]).decode('utf-8') 
+    
+    new_psico = Psicologo(email=body["email"], role=body["role"],  
+                    password=encrypted_password, is_active=True)
+
+    if "first_name" in body:
+        new_psico.first_name = body["first_name"]
+    else:
+        new_psico.first_name = ""
+
+    if "last_name" in body:
+        new_psico.last_name = body["last_name"]
+    else:
+        new_psico.last_name = ""
+    
+    if "role" in body:
+        new_psico.role = body["role"]
+    else:
+        new_psico.role = ""
+
+    db.session.add(new_psico)
+    db.session.commit()
+
+    return jsonify({"msg":"ok"})
+
+@api.route('/login', methods=['POST'])
+def user_login():
+    body=request.get_json()
+    #1. Valido los campos del body de la petición 
+    if "email" not in body:
+        return jsonify({"msg":"El campo email es requerido"}), 400
+    if "password" not in body:
+        return jsonify({"msg":"El campo password es requerido"}), 400
+    
+    #2. Busco al usuario en la base de datos con el correo 
+    user=User.query.filter_by(email=body["email"]).first()
+
+    #2.1 si el usuario no aparece retorna un error 404
+    if user is None:
+        return jsonify({"msg": "Usuario no encontrado"}), 404
+    
+    #3. verifico el campo password del body con el password del usuario en la base de datos 
+    password_checked=bcrypt.check_password_hash(user.password,body["password"])
+
+    #3.1 si no se verifica se retorna un error de clave invalidad se retorna un error 401
+    if password_checked == False:
+        return jsonify({"msg": "Clave invalida"}), 401
+    
+    #4. Generar el token
+    
+    token = create_access_token(identity=user.id, additional_claims={"role": role})
+    return jsonify({"token":token}), 200
+
+@api.route('/logout', methods=['POST'] )
+@jwt_required() 
+def user_logout():
+    jti=get_jwt()["jti"]
+    token_blocked=TokenBlockedList(jti=jti)
+    db.session.add(token_blocked)
+    db.session.commit()
+    return jsonify({"msg": "Sesión Cerrada"})
+
+
+@api.route("/userinfo", methods=['GET'])
+@jwt_required() 
+def user_info():
+    user=get_jwt_identity()
+    payload=get_jwt()
+    return jsonify({"user":user,"role":payload["role"]})
