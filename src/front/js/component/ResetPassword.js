f11d616938f13c66f58f06af694ca41f31c67fda
Eduardo Ribeiro <166298206+EIAHRJAY@users.noreply.github.com>
2024-08-20 02:55:22 +0000

    update restabelecer contrasena

--- src/front/js/component/ResetPassword.js
+++ src/front/js/component/ResetPassword.js
@@ -1,43 +1,111 @@
-import React from "react";
+import React ,{useState}from "react";
 import { useContext } from "react"; 
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
+import "../../styles/resetemail.css";
+import { BsLock } from "react-icons/bs";
 
-export const ResetPassword= () => {
+export const ResetPassword= ({ token }) => {
     const { store, actions } = useContext(Context);
     const navigate = useNavigate(); // Hook de navegación
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        if (password !== confirmPassword) {
+          alert("Las contraseñas no coinciden");
+          return;
+        }
+    
+        try {
+          const response = await fetch(`https://crispy-couscous-wrvj697556rp29r66-3001.app.github.dev/api/reset_password`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              reset_token: token,
+              new_password: password
+            })
+          });
+    
+          if (!response.ok) {
+            const errorData = await response.json();
+            console.error("Error:", errorData.message);
+            alert(`Error: ${errorData.message}`);
+          } else {
+            const result = await response.json();
+            console.log(result.message);
+            alert("Contraseña restablecida con éxito. Ahora puedes iniciar sesión.");
+            navigate("/login"); // Redirige al usuario a la página de inicio de sesión
+          }
+        } catch (error) {
+          console.error("Fetch error:", error);
+          alert("Ocurrió un problema al intentar restablecer la contraseña.");
+        }
+   
+    } 
 
     return(
-<div className="container">
-    <form>
-        <div className="mb-3">
-            <label htmlFor="exampleInputPassword" className="form-label">
-                Contraseña
-            </label>
-            <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword"
-            />
+       
+        <div className="container d-flex">
+        <div className=" rest ">
+            <div className="resetclass">
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                        <br/>
+                            <h2 > Modificación de Contraseña</h2>
+                        <br/>
+                        <p className="titlerest">Por favor, ingresa y confirme su nueva contraseña.</p>
+
+                            <div className="titlerest">
+                                <label htmlFor="newpassword" className="form-label mt-3">
+                                <BsLock /> Nueva Contraseña
+                                </label>
+                            </div>
+                                <input
+                                name="password"
+                                type="password"
+                                className="form-control inputrest"
+                                 id="newpassword"
+                                 value={password}
+                                 onChange={(e) => setPassword(e.target.value)}
+                                
+                                    />
+
+                                <div className="titlerest">
+                                <label htmlFor="confirmPassword" className="form-label mt-3">
+                                <BsLock /> Confirma la Contraseña
+                                </label>
+                            </div>
+                                <input
+                                name="confirmPassword"
+                                type="password"
+                                className="form-control inputrest"
+                                 id="confirmPassword"
+                                 value={confirmPassword}
+                                 onChange={(e) => setConfirmPassword(e.target.value)}
+                                
+                                    />
+                        </div>
+                    <div className="d-flex justify-content-around">
+                        
+                        <button
+                            type="submit"
+                            className="btn btn-outline-primary  buttonemail "
+                        >
+                            Enviar
+                        </button>
+
+                    </div>
+                   
+                    <br/>
+                    </form>
+                </div>
         </div>
-        <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">
-                Repite la Contraseña
-            </label>
-            <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-            />
-        </div>
-        <button
-        type="submit"
-        className="btn btn-outline-primary submit-login mb-2"
-        >
-        Enviar
-        </button>
-  </form>      
-</div>
+    </div>
     );
 };
 
